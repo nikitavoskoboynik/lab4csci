6581341e5927eb3ee92acacfc7bd65b794db632f
nikitavoskoboynik <nikitavosk@gmail.com>
2014-09-17 15:49:54 -0600

    Update hello.java

--- src/hello.java
+++ src/hello.java
@@ -2,5 +2,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); //Display the string.
         System.out.println("How are you?");
+        System.out.println("I'm done with lab4");
 	}
 }
